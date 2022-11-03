e6b970184def9bdcae706e11d505a9eea18c277f
serraguti <pacoserranox@gmail.com>
2022-11-03 10:12:57 +0100

    commit

---
+++ src/components/CicloVida.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h1>Hooks VUE</h1>
+        <button @click="cambiarSaludo()">
+            Mostrar saludo
+        </button>
+        <button v-on:click="sdsdad">
+            
+        </button>
+        <h2 style="background-color:white;color:red">
+            {{saludo}}
+        </h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CicloVida",
+        data() {
+            return {
+                saludo: "Hoy es juernes"
+            }
+        },
+        methods: {
+            //AQUI INCLUIMOS NUESTROS METODOS PERSONALIZADOS
+            cambiarSaludo() {
+                //TODA VARIABLE O METODO DEBEMOS LLAMARLO CON this
+                this.saludo = "Y mañana viernes!!!";
+            }
+        }, 
+        mounted() {
+            console.log("Component montado");
+        }, 
+        created() {
+            console.log("Componente creado");
+        }, 
+        unmounted() {
+            console.log("Componente destruido");
+        }, 
+        updated() {
+            console.log("Componente modificado");
+        }
+    }
+</script>
+
+<style>
+
+</style>
