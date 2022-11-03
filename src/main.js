d8cfda6d4285ad4213cd751b7ba49b262ccd2431
serraguti <pacoserranox@gmail.com>
2022-11-03 13:01:20 +0100

    commit

--- src/main.js
+++ src/main.js
@@ -10,6 +10,14 @@ app.config.globalProperties.$filters = {
     //AQUI LOS METODOS/FILTERS GLOBALES PARA LA APP
     mayuscula(dato) {
         return dato.toUpperCase();
+    },
+    getNumeroDoble(numero){
+        return numero * 2;
+    },
+    getMultiplicacion(numero1, numero2) {
+        //5,1
+        //5,2
+        return numero1 * numero2;
     }
 }
 app.use(router).mount('#app');
